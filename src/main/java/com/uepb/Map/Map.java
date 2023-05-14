fd4c5d43e509144b422b4cda4c0ff24876bd80e8
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-05-14 17:30:40 -0300

    Criada a classe Map e MapTests.java

---
+++ src/main/java/com/uepb/Map/Map.java
@@ -0,0 +1,11 @@
+package com.uepb.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Map {
+	public static void main(String[] args) {
+		SpringApplication.run(Map.class, args);
+	}
+}
