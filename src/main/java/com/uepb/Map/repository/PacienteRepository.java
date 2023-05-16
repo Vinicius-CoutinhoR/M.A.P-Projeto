f5ef3ac124c4ac0cbd66f524566dc347ede76c7c
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-05-16 14:24:34 -0300

    Adicionado o PacienteService e atualizado o repository

--- src/main/java/com/uepb/Map/repository/PacienteRepository.java
+++ src/main/java/com/uepb/Map/repository/PacienteRepository.java
@@ -1,4 +1,7 @@
 package com.uepb.Map.repository;
 
-public interface PacienteRepository {
+import com.uepb.Map.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
 }
