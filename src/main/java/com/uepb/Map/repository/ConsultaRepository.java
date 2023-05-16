30354dc67c4634355015b5b70648303385398732
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-05-16 14:27:17 -0300

    Adicionado o ConsultaRepository.java

---
+++ src/main/java/com/uepb/Map/repository/ConsultaRepository.java
@@ -0,0 +1,6 @@
+package com.uepb.Map.repository;
+
+import com.uepb.Map.model.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository  extends JpaRepository<Consulta, Integer> {}
