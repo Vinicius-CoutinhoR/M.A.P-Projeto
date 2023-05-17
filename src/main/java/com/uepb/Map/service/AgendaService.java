c16b4c69d59e417e02a7315bb5d3c9b6aa021c20
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-05-17 20:59:23 -0300

    Adicionado o AgendaService.java

--- src/main/java/com/uepb/Map/service/AgendaService.java
+++ src/main/java/com/uepb/Map/service/AgendaService.java
@@ -5,19 +5,28 @@ import com.uepb.Map.repository.AgendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class AgendaService {
 
     @Autowired
     AgendaRepository agendaRepository;
 
+    //Find methods
+    public List<Agenda> findAll() { return agendaRepository.findAll(); }
+
+    public Optional<Agenda> findById(Integer id) { return agendaRepository.findById(id); }
+
+    //Save methods
     public void save(Agenda agenda) {
-        agendaRepository.saveAndFlush(agenda);
+        agendaRepository.save(agenda);
     }
 
-    public void saveAll(Agenda agenda) {
-        agendaRepository.saveAndFlush(agenda);
-    }
+    public void saveAndFlush(Agenda agenda) { agendaRepository.saveAndFlush(agenda); }
 
+    public void saveAll(List<Agenda> agendaList) { agendaRepository.saveAll(agendaList); }
 
+    public void saveAllAndFlush(List<Agenda> agendaList) { agendaRepository.saveAllAndFlush(agendaList); }
 }
