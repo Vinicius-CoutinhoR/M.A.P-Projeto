752d179f34dfd8cbef2c3e50e092660e3f9a519c
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-05-17 20:58:30 -0300

    Adicionado o MedicoService.java

--- src/main/java/com/uepb/Map/service/MedicoService.java
+++ src/main/java/com/uepb/Map/service/MedicoService.java
@@ -1,9 +1,13 @@
 package com.uepb.Map.service;
 
+import com.uepb.Map.model.Consulta;
 import com.uepb.Map.model.Medico;
 import com.uepb.Map.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedicoService {
@@ -11,9 +15,30 @@ public class MedicoService {
     @Autowired
     MedicoRepository medicoRepository;
 
+    //Find methods
+    public List<Medico> findAll() {
+        return medicoRepository.findAll();
+    }
+
+    public Optional<Medico> findById(Integer id) {
+        return medicoRepository.findById(id);
+    }
+
+    //Save methods
     public void save(Medico medico) {
+        medicoRepository.save(medico);
+    }
+
+    public void saveAndFlush(Medico medico) {
         medicoRepository.saveAndFlush(medico);
     }
 
+    public void saveAll(List<Medico> medicoList) {
+        medicoRepository.saveAll(medicoList);
+    }
+
+    public void saveAllAndFlush(List<Medico> medicoList) {
+        medicoRepository.saveAllAndFlush(medicoList);
+    }
 
 }
