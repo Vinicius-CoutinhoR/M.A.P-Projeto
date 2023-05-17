a86bd3ef22dbede21f71dacd4e6d59e2f7ac7a00
Vinicius-CoutinhoR <coutinhocp07@gmail.com>
2023-05-17 20:58:55 -0300

    Adicionado o RecepcionistaService.java

--- src/main/java/com/uepb/Map/service/RecepcionistaService.java
+++ src/main/java/com/uepb/Map/service/RecepcionistaService.java
@@ -1,4 +1,41 @@
 package com.uepb.Map.service;
 
+import com.uepb.Map.model.Paciente;
+import com.uepb.Map.model.Recepcionista;
+import com.uepb.Map.repository.RecepcionistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class RecepcionistaService {
+
+    @Autowired
+    RecepcionistaRepository recepcionistaRepository;
+
+    //Find methods
+    public List<Recepcionista> findAll() {
+        return recepcionistaRepository.findAll();
+    }
+
+    public Optional<Recepcionista> findById(Integer id) {
+        return recepcionistaRepository.findById(id);
+    }
+
+    //Save methods
+    public void save(Recepcionista recepcionista) {
+        recepcionistaRepository.save(recepcionista);
+    }
+
+    public void saveAndFlush(Recepcionista recepcionista) {
+        recepcionistaRepository.saveAndFlush(recepcionista);
+    }
+
+    public void saveAll(List<Recepcionista> recepcionistaList) {
+        recepcionistaRepository.saveAll(recepcionistaList);
+    }
+
+    public void saveAllAndFlush(List<Recepcionista> recepcionistaList) {
+        recepcionistaRepository.saveAllAndFlush(recepcionistaList);
+    }
 }
